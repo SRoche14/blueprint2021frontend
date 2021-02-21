02807f948b1e551f97bbf1ed631fc9d0f8b59889
Steven Roche <67879499+SRoche14@users.noreply.github.com>
2021-02-21 14:55:12 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,11 +1,5 @@
-
-chrome.tabs.executeScript( {
-    code: "window.getSelection().toString();"
-}, function(selection) {
-    document.getElementById("output").innerHTML = selection[0];
-});
 document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('button').addEventListener('click', onclick, false)
+  document.querySelector('#startbtn').addEventListener('click', onclick, false)
   function onclick () {
 
     chrome.tabs.query({currentWindow: true, active: true},
