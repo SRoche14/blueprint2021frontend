a7e0edfcc65a4cf0192f64da8e7baeb59ee2825e
CarolineBaillie <caroline@bailliefamily.com>
2021-02-21 15:14:02 -0500

    questions appear

--- questions.js
+++ questions.js
@@ -1,7 +1,35 @@
-const url = 'http://127.0.0.1:5000/getQs'
-fetch(url)
+const url1 = 'http://127.0.0.1:5000/getQs'
+var total;
+fetch(url1)
 .then(response => response.json())  
 .then(json => {
-    // this is the response from flask
-    console.log(json);
-})
+    total = json["data"];
+    console.log(total);
+    var questions = total[0];
+var answers = total[1];
+for (var i = 0; i < questions.length; i++) {
+    console.log(questions[i]);
+    console.log(answers[i]);
+    var div = document.createElement("div");
+
+    var q = document.createElement("H2");
+    q.innerHTML = "Question: ";
+    div.appendChild(q);
+
+    var ques = document.createElement("P");
+    ques.innerHTML = questions[i];
+    div.appendChild(ques);
+
+    var a = document.createElement("H2");
+    a.innerHTML = "Answer: ";
+    div.appendChild(a);
+
+    var txtArea = document.createElement("TEXTAREA");
+    div.appendChild(txtArea);
+
+    document.body.appendChild(div);
+
+  }
+});
+
+console.log(document.getElementById("output"));
