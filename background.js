dc699a1f7e708d953967a4b9a7877230f33fda95
Steven Roche <67879499+SRoche14@users.noreply.github.com>
2021-02-21 12:31:24 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,22 +1,11 @@
-//window.bears = {}
-//chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-//  bears[request.url] = request.count
-//})
-
-//chrome.browserAction.onClicked.addListener(function (tab) {
-//  chrome.tabs.create({url: 'quiz.html'})
-//})
-chrome.tabs.onActivated.addListener(tab => {
-  chrome.tabs.get(tab.tabId, current_tab_info => {
-    chrome.tabs.insertCSS(null, {file: 'style.css'});
-    chrome.tabs.executeScript(null, {file: 'foreground.js'}, () => console.log('i injected'));
-  });
+chrome.commands.onCommand.addListener(function (command) {
+    if (command === "save") {
+      chrome.tabs.executeScript( {
+    code: "window.getSelection().toString();"
+}, function(selection) {
+    let data = selection[0];
+    console.log(data);
 });
 
-//chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//  if(request.message === "Data Set.") {
-  //  chrome.storage.local.get("sentence", value => {
-  //    console.log(value)
-  //  });
-//  }
-//});
+    }
+});
