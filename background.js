e0c6d3044ca3e711ff133427e7284647a3b23c43
CarolineBaillie <caroline@bailliefamily.com>
2021-02-21 13:39:53 -0500

    Sends to flask once selected

--- background.js
+++ background.js
@@ -6,6 +6,14 @@ chrome.commands.onCommand.addListener(function (command) {
     let data = selection[0];
     chrome.storage.local.set({"sentence": data});
     console.log(data);
+    var url = "http://127.0.0.1:5000/";
+    fetch(url, {
+        method: 'POST',
+        mode: 'no-cors',
+        body: JSON.stringify({
+            data: data
+        })
+    })
 });
 
     }
