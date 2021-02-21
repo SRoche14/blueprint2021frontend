97b06e1e9ce5d3b24fe55b9b56199fe6dbde4735
Steven Roche <67879499+SRoche14@users.noreply.github.com>
2021-02-21 16:59:06 -0500

    Add files via upload

---
+++ appear.js
@@ -0,0 +1,7 @@
+alert();
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+$('#answerbtn').on('click', function () {
+  alert();
+  $('.questionbox2').css('visibility', 'visible');
+  $('.questionbox2').css('display', 'block');
+});
